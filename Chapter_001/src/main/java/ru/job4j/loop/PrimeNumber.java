1a07bb1066b7a4eb5959ddbaa56215c38e50b069
Sukhareva-lubov <sukhareva.lubov@mail.ru>
2019-09-03 23:49:28 +0300

    5.5. Простые числа.[#160663]

---
+++ Chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,24 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+    public int calc(int finish) {
+        int count = 0;
+        for (int i = 2; i <= finish; i++){
+            int a = 0;
+            for (int j = 2; j <= i ; j++){
+
+                if (i % j == 0) {
+                        a++;
+                    }
+            }
+
+            if (a < 2 ) {
+                count ++;
+            }
+
+        }
+
+
+        return count;
+    }
+}
