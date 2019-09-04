46b6d2b337d90ed77ac362e670013be9ac4f9697
Sukhareva-lubov <sukhareva.lubov@mail.ru>
2019-09-04 11:14:34 +0300

    5.6. Ипотека.[#160664]

---
+++ Chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount, int monthly, double percent) {
+        int year = 0;
+        double dolg = ((percent/100) + 1.0) * amount;
+        double payYear = 12 * monthly ;
+        while ( dolg > 0 ) {
+            dolg = dolg - payYear ;
+            year ++;
+        }
+
+        return year;
+    }
+}
