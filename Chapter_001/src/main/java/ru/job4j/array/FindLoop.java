d3368d2e09fce895cd785a40574e360915d448ab
Sukhareva-lubov <sukhareva.lubov@mail.ru>
2019-09-12 12:53:59 +0300

    6.5.1. Сортировка выборкой.[#160679]

--- Chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ Chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -26,4 +26,18 @@ public class FindLoop {
 
         return rst;
     }
+
+    public int[] sort(int[] data) {
+        for (int i=0; i < (data.length-1); i++) {
+            for (int j=i+1; j < data.length ; j++) {
+                if (data[i] > data[j]) {
+                    int temp;
+                    temp = data[i];
+                    data[i] = data[j];
+                    data[j] = temp;
+                }
+            }
+        }
+        return data;
+    }
 }
--- Chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ Chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -4,37 +4,8 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+
 public class FindLoopTest {
-
-    @Test
-    public void whenArrayHas5Then0() {
-        FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3};
-        int value = 5;
-        int result = find.indexOf(input, value);
-        int expect = 0;
-        assertThat(result, is(expect));
-    }
-
-    @Test
-    public void whenArrayHas4Then3() {
-        FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3, 4, 7};
-        int value = 4;
-        int result = find.indexOf(input, value);
-        int expect = 3;
-        assertThat(result, is(expect));
-    }
-
-    @Test
-    public void whenArrayHas1Then() {
-        FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3, 7, 0};
-        int value = 1;
-        int result = find.indexOf(input, value);
-        int expect = -1;
-        assertThat(result, is(expect));
-    }
 
     @Test
     public void whenArrayHasLengh5Then0() {
@@ -59,14 +30,11 @@ public class FindLoopTest {
     }
 
     @Test
-    public void whenFind4() {
+    public void whenSort5() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 2, 10, 2, 4};
-        int value = 4;
-        int start = 1;
-        int finish = 7;
-        int result = find.indexOf(input, value, start, finish);
-        int expect = 4;
+        int[] input = new int[] {3, 4, 1, 2, 5};
+        int result[] = find.sort(input);
+        int[] expect = new int[] {1, 2, 3, 4, 5};
         assertThat(result, is(expect));
     }
 }
