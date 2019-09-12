ae15ca4f25288b8ae47bb35e82ed42897cb5e83f
Sukhareva-lubov <sukhareva.lubov@mail.ru>
2019-09-12 10:02:11 +0300

    6.4.1. Слово заканчивается с ...[#160677]

---
+++ Chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+        // проверить. что массив word имеет последние элементы одинаковые с post
+        int k = (word.length-1);
+        for (int i= (post.length - 1); i >= 0 ;i--) {
+            if (word[k] != post[i]) {
+                  result = false;
+                  break;
+              }
+              k--;
+
+        }
+        return result;
+    }
+}
