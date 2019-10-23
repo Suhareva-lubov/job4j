bba3e3afab4fb7f8779b71d6ea478d2a89f44549
Sukhareva-lubov <sukhareva.lubov@mail.ru>
2019-10-23 18:18:40 +0300

    4. Расстояние между точками в трехмерном пространстве.[#160779]

--- Chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ Chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -4,10 +4,17 @@ public class Point {
 
     private int x;
     private int y;
+    private int z;
 
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
+    }
+
+    public Point (int first, int second, int third) {
+        this.x = first;
+        this.y = second;
+        this.z = third;
     }
 
     public double distance(Point that) {
@@ -17,21 +24,24 @@ public class Point {
         return d;
     }
 
-    /*public static void main(String[] args) {
-        double result = distance(0, 0, 2, 0);
-        System.out.println("Return (0.0) to (2.0) " + result);
+    public double distance3d(Point that) {
+        double a = Math.pow(that.x - this.x, 2);
+        double b = Math.pow(that.y - this.y, 2);
+        double c = Math.pow(that.z - this.z, 2);
+        return Math.sqrt(a + b + c);
+    }
 
-        double result1 = distance(1, 0, 2, 3);
-        System.out.println("Return (1.0) to (2.3) " + result1);
-
-        double result2 = distance(3, 1, 1, 2);
-        System.out.println("Return (3.1) to (1.2) " + result2);
-    }*/
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+    }
 
     public static void main(String[] args) {
         Point a = new Point(0, 0);
         Point b = new Point(0, 2);
+        a.info();
+        b.info();
         double dist = a.distance(b);
         System.out.println(dist);
+
     }
 }
--- Chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ Chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -14,7 +14,24 @@ public class PointTest {
         double expected = 3.16;
         double out = x1.distance(y2);
         Assert.assertEquals(expected, out, 0.01);
+    }
 
+    @Test
+    public void distance3D() {
+        Point x = new Point(1, 5, 4);
+        Point y = new Point(1, 2, 0);
+        double expected = 5.0;
+        double out = x.distance3d(y);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void distance3Dtest2() {
+        Point x = new Point(4, 5, 0);
+        Point y = new Point(7, 2, 1);
+        double expected = 4.35;
+        double out = x.distance3d(y);
+        Assert.assertEquals(expected, out, 0.01);
     }
 
 }
