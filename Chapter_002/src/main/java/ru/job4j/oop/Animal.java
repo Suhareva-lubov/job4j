99a8bc0e5a5defd53e924456adce2a7788965d56
Sukhareva-lubov <sukhareva.lubov@mail.ru>
2019-10-28 19:27:38 +0300

    2. Перегрузка конструктора. [#173890]

--- Chapter_002/src/main/java/ru/job4j/oop/Animal.java
+++ Chapter_002/src/main/java/ru/job4j/oop/Animal.java
@@ -1,8 +1,9 @@
 package ru.job4j.oop;
 
 public class Animal {
-    public Animal () {
-        super();
-        System.out.println("Animal");
+    String name;
+
+    public Animal (String name) {
+        this.name = name;
     }
 }
