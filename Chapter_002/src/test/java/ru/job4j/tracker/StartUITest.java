ed46ffbcc49d6388f2570ecfa8cc2895eec78212
Sukhareva-lubov <sukhareva.lubov@mail.ru>
2020-02-18 18:50:45 +0300

    5.1. Тестирование. Подготовка данных. [#182960]

--- Chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ Chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 
 public class StartUITest {
     @Test
-    public void whenAddItem() {
+    public void whenCreatedItem() {
         String[] answers = {"Fix PC"};
         Input input = new StubInput(answers);
         Tracker tracker = new Tracker();
@@ -16,6 +16,38 @@ public class StartUITest {
         Item created = tracker.findAll()[0];
         Item expected = new Item ("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
+    }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                "replaced item",
+                item.getId() //id сохраненной заявки в обьект tracker
+        };
+        StartUI.replaceItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("First item");
+        Item item1 = new Item("Second item");
+        tracker.add(item);
+        tracker.add(item1);
+        String[] answers = {
+                item.getId()
+        };
+        StartUI.deteleItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        String a = null;
+
+        assertThat(replaced, is(a));
+
     }
 
 }
